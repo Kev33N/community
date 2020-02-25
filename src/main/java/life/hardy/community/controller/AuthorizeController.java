739b1791620c85ad30c08a5119006d34c4994750
Kev33N <Kevin_ker@163.com>
2020-02-25 09:57:39 +0800

    将固定写法改为yml配置文件

--- src/main/java/life/hardy/community/controller/AuthorizeController.java
+++ src/main/java/life/hardy/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.hardy.community.dto.AccessTokenDTO;
 import life.hardy.community.dto.GithubUser;
 import life.hardy.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,17 +15,26 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client_id}")
+    private String clientId;
+
+    @Value("${github.client_secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect_uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
+
         AccessTokenDTO accessTokenDto = new AccessTokenDTO();
-        accessTokenDto.setClient_id("b61df0ff427d7a0af9c8");
-        accessTokenDto.setClient_secret("b463d40288c7ee468528ef24a178496a7f6d6656");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        System.out.println("accessToken:"+accessToken);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user);
         return "index";
