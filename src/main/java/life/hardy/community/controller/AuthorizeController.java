06fb8d63b58c95c14b9bafc48d44cee959f1b587
Kev33N <Kevin_ker@163.com>
2020-02-25 21:41:05 +0800

    update

--- src/main/java/life/hardy/community/controller/AuthorizeController.java
+++ src/main/java/life/hardy/community/controller/AuthorizeController.java
@@ -1,13 +1,20 @@
 package life.hardy.community.controller;
 
+import life.hardy.community.domain.User;
 import life.hardy.community.dto.AccessTokenDTO;
 import life.hardy.community.dto.GithubUser;
+import life.hardy.community.dto.LocalUser;
+import life.hardy.community.mapper.UserMapper;
 import life.hardy.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,9 +31,13 @@ public class AuthorizeController {
     @Value("${github.redirect_uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
 
         AccessTokenDTO accessTokenDto = new AccessTokenDTO();
         accessTokenDto.setClient_id(clientId);
@@ -35,8 +46,37 @@ public class AuthorizeController {
         accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user);
-        return "index";
+        System.out.println(accessToken);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser);
+        if (githubUser!=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
+
+    }
+
+    @RequestMapping("/user/login")
+    public String loginTest(String username,String password,HttpServletRequest request){
+        User user = new User();
+        user.setToken(UUID.randomUUID().toString());
+        user.setName(username);
+        user.setAccountId(password);
+        user.setGmtCreate(System.currentTimeMillis());
+        user.setGmtModified(user.getGmtCreate());
+        userMapper.insert(user);
+
+        request.getSession().setAttribute("user",user);
+        System.out.println(username+":"+password);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/life/hardy/community/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package life.hardy.community.mapper;
+
+import life.hardy.community.domain.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper
+@Component
+public interface UserMapper {
+
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
