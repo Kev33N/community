55342340475a0007d82090f5877573e212f3f971
Kev33N <Kevin_ker@163.com>
2020-02-26 17:46:29 +0800

    add publish function

---
+++ src/main/java/life/hardy/community/controller/PublishController.java
@@ -0,0 +1,38 @@
+package life.hardy.community.controller;
+
+import life.hardy.community.domain.Question;
+import life.hardy.community.domain.User;
+import life.hardy.community.mapper.QuestionMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(Question question, HttpServletRequest request, Model model){
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null){
+            model.addAttribute("error","当前用户未登录");
+            return "publish";
+        }
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+        questionMapper.create(question);
+        return "redirect:/";
+    }
+}
